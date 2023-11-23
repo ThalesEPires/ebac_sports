fdbc0aeecbe4f362fd15881b21064637e1bb3d6a
Thales Pires <thaedupi@gmail.com>
2023-11-22 21:11:18 -0300

    concluindo primeira parte do exercicio

--- src/components/Produto/index.tsx
+++ src/components/Produto/index.tsx
@@ -1,39 +1,35 @@
-import { Produto as ProdutoType } from '../../App'
+import { useDispatch } from 'react-redux'
 import * as S from './styles'
 
-type Props = {
-  produto: ProdutoType
-  aoComprar: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-  estaNosFavoritos: boolean
-}
+import { adicionar } from '../../store/reducers/carrinho'
+import { useGetProdutosQuery } from '../../services/api'
 
 export const paraReal = (valor: number) =>
   new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
     valor
   )
 
-const ProdutoComponent = ({
-  produto,
-  aoComprar,
-  favoritar,
-  estaNosFavoritos
-}: Props) => {
+const ProdutoComponent = () => {
+  const { data: produto, isLoading } = useGetProdutosQuery()
+  const dispatch = useDispatch()
+
+  if (isLoading) return <h2>Carregando...</h2>
+
   return (
     <S.Produto>
       <S.Capa>
-        <img src={produto.imagem} alt={produto.nome} />
+        <img src={produto?.imagem} alt={produto?.nome} />
       </S.Capa>
-      <S.Titulo>{produto.nome}</S.Titulo>
+      <S.Titulo>{produto?.nome}</S.Titulo>
       <S.Prices>
-        <strong>{paraReal(produto.preco)}</strong>
+        <strong>{paraReal(produto?.preco)}</strong>
       </S.Prices>
       <S.BtnComprar onClick={() => favoritar(produto)} type="button">
         {estaNosFavoritos
           ? '- Remover dos favoritos'
           : '+ Adicionar aos favoritos'}
       </S.BtnComprar>
-      <S.BtnComprar onClick={() => aoComprar(produto)} type="button">
+      <S.BtnComprar onClick={() => dispatch(adicionar(produto?))} type="button">
         Adicionar ao carrinho
       </S.BtnComprar>
     </S.Produto>
